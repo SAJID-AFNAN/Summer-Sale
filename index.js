b55a2c71a9184c44424ff0a8fc980d17dcfadd14
SAJID-AFNAN <sajidafnan91@gmail.com>
2023-08-29 00:53:38 +0600

    added JS

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function handlclick(data) {
     const previousPricestring = previousPrice.innerText;
 
     const TotalPrice = parseFloat(amount) + parseFloat(previousPricestring);
-    previousPrice.innerText = TotalPrice;
+    previousPrice.innerText = TotalPrice.toFixed(2);
 
     const accesoriesName = data.children[1].children[1].children[0].innerText;
 
@@ -17,15 +17,43 @@ function handlclick(data) {
     li.innerText = accesoriesName;
     List.appendChild(li);
 
-    if (TotalPrice >= 200) {
+    if (TotalPrice >= 0) {
         document.getElementById('Make-btn').removeAttribute("disabled")
     }
 
+    if (TotalPrice >= 200) {
+        document.getElementById('Apply-btn').removeAttribute("disabled");
+    }
+
+    const Total = document.getElementById('Total');
+    Total.innerText = TotalPrice.toFixed(2);
     // const couponItem = document.getElementById('coupon-input');
     // const coupon = couponItem.value;
     // if (coupon == 'SELL200') {
     //     document.getElementById('Apply-btn').removeAttribute('disabled')
     // }
     // coupon = '';
-
 }
+
+document.getElementById('Apply-btn').addEventListener('click', function () {
+
+    const couponItem = document.getElementById('coupon-input');
+    const coupon = couponItem.value;
+
+    const Price = document.getElementById('Total-price');
+    const TotalPricestring = Price.innerText;
+    const TotalPrice = parseFloat(TotalPricestring);
+
+    const Discount = document.getElementById('discount');
+
+    if (coupon == 'SELL200') {
+        const Dis = ((TotalPrice * 20) / 100)
+        Discount.innerText = Dis.toFixed(2);
+        console.log(Dis);
+    }
+    console.log(TotalPrice);
+
+    // console.log("kaj koer")
+
+
+})
